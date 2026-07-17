2877f046c592d73ea1c2aa6d20a6460dcd1bbc62
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2026-07-16 21:59:01 -0300

    test(app): assert router outlet rendering

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
 import { App } from './app';
 
 describe('App', () => {
@@ -14,10 +16,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render router outlet', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tennis-score');
+    const outlet = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(outlet).toBeTruthy();
   });
 });
